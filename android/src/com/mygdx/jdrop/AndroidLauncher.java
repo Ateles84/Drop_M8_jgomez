49a76b8c55230e4cc358af7e4d5157a810abb171
Joan <juanbass84@gmail.com>
2020-01-14 16:33:09 +0100

    joc base fet

---
+++ android/src/com/mygdx/jdrop/AndroidLauncher.java
@@ -0,0 +1,20 @@
+package com.mygdx.jdrop;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.jdrop.JDrop;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+		config.useAccelerometer = false;
+		config.useCompass = false;
+
+		initialize(new JDrop(), config);
+	}
+}
---
+++ desktop/src/com/mygdx/jdrop/desktop/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.mygdx.jdrop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.jdrop.DropJoan;
+import com.mygdx.jdrop.JDrop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		//Modifiquem les mesures de la finestra en desktop
+		config.width = 800;
+		config.height = 480;
+
+		new LwjglApplication(new DropJoan(), config);
+	}
+}
